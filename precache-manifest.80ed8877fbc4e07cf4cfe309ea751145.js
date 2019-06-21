b8604afad78e900288cc399746ab72ca3c3b30d7
Darken141 <47373497+Darken141@users.noreply.github.com>
2019-06-21 17:10:16 +0100

    Updates

---
+++ precache-manifest.80ed8877fbc4e07cf4cfe309ea751145.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa87121b88c1a4a6ecb5ad59b0184a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8446bca1745d2551c91e",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "f1dec631432945bf3ba1",
+    "url": "/static/css/main.d99e285c.chunk.css"
+  },
+  {
+    "revision": "8446bca1745d2551c91e",
+    "url": "/static/js/2.df614183.chunk.js"
+  },
+  {
+    "revision": "f1dec631432945bf3ba1",
+    "url": "/static/js/main.b61876a9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4fa4780bef708bb07c70cd7550d2cb88",
+    "url": "/static/media/DuskLogo.4fa4780b.png"
+  }
+]);
